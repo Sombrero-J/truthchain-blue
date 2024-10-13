12eb0fa95d82137a732ef15fd0772f67ab1f618d
Weng Chan <wengchan99899.com>
2024-10-13 14:43:52 +0100

    try again

--- src/lib/server/pinata.ts
+++ src/lib/server/pinata.ts
@@ -2,8 +2,6 @@ import { PinataSDK } from 'pinata-web3';
 import { PINATA_JWT } from '$env/static/private';
 import { PUBLIC_GATEWAY_URL } from '$env/static/public';
 
-import fs from 'fs';
-
 export const pinata = new PinataSDK({
 	pinataJwt: `${PINATA_JWT}`,
 	pinataGateway: `${PUBLIC_GATEWAY_URL}`
